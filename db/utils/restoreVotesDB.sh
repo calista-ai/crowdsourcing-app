944ca746331ba20b3d6ab07d840b4d311a114cf4
Alex Delitzas <alex.delitzas@gmail.com>
2020-04-14 20:55:39 +0300

    Update database restore script

--- db/utils/restoreVotesDB.sh
+++ db/utils/restoreVotesDB.sh
@@ -1,6 +1,5 @@
 #! /bin/bash
 
-dataToRestore="../backup/17_04_2019__13_52_11/votes/";
 mongorestore --drop --authenticationDatabase admin \
   -u $MONGO_INITDB_ROOT_USERNAME -p $MONGO_INITDB_ROOT_PASSWORD \
-  --db votes $dataToRestore
+  --db votes $1
